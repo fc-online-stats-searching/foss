6c7ad49f6dc0afea9d40572f090842f49bc1341c
woogi <kimjinuk1999@gmail.com>
2023-11-27 18:46:37 +0900

    feat: 홈 화면을 구성한다.

---
+++ android/app/src/main/java/com/foss/foss/feature/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.foss.foss.feature.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.foss.foss.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
