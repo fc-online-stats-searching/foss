21a1c8cfd3a10db1e6bb7bc3bf3ba69c4bdc0390
krrong <khk9664@naver.com>
2023-12-02 15:24:39 +0900

    feat: data binding을 사용하도록 변경

--- android/app/src/main/java/com/foss/foss/feature/statsearching/relative/RelativeStatsFragment.kt
+++ android/app/src/main/java/com/foss/foss/feature/statsearching/relative/RelativeStatsFragment.kt
@@ -5,15 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.foss.foss.R
+import com.foss.foss.databinding.FragmentRelativeStatsBinding
 
 class RelativeStatsFragment : Fragment() {
+    private var _binding: FragmentRelativeStatsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_relative_stats, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentRelativeStatsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
