e8c7c450cc5e6d4565854134219bcd5baa5b7f65
krrong <khk9664@naver.com>
2023-12-05 16:47:43 +0900

    feat: 상대 전적 뷰를 구현한다 (#16)
    * feat: data binding을 사용하도록 변경
    
    * style: 텍스트 기본 색상을 흰색으로 변경
    
    * feat: 검색이 필요하다는 문구를 띄움
    
    * style: 색상명 변경
    
    * feat: 상대전적 뷰 구현
    
    * feat: 상대전적 뷰에서 보여줄 UiModel 구현
    
    * feat: 상대전적에서 사용할 Adapter, ViewHolder 구현
    
    * feat: 상대전적 뷰에 임시 데이터로 넣어 확인 가능하도록 구현
    
    * refactor: 클래스명 변경
    
    * refactor: 데이터바인딩을 뷰바인딩을 사용하도록 변경
    
    * refactor: 뷰홀더 인스턴스 반환을 동반객체에서 하도록 변경
    
    * chore: ktlint 버전 변경

---
+++ android/app/src/main/java/com/foss/foss/feature/statsearching/relative/RelativeStatsAdapter.kt
@@ -0,0 +1,20 @@
+package com.foss.foss.feature.statsearching.relative
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.foss.foss.model.RelativeStatsUiModel
+
+class RelativeStatsAdapter(private val relativeStats: List<RelativeStatsUiModel>) :
+    RecyclerView.Adapter<RelativeStatsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RelativeStatsViewHolder {
+        return RelativeStatsViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: RelativeStatsViewHolder, position: Int) {
+        holder.bind(relativeStats[position])
+    }
+
+    override fun getItemCount(): Int {
+        return relativeStats.size
+    }
+}
---
+++ android/app/src/main/java/com/foss/foss/feature/statsearching/relative/RelativeStatsViewHolder.kt
@@ -0,0 +1,37 @@
+package com.foss.foss.feature.statsearching.relative
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.foss.foss.R
+import com.foss.foss.databinding.ItemRelativeStatsBinding
+import com.foss.foss.model.RelativeStatsUiModel
+
+class RelativeStatsViewHolder private constructor(
+    private val binding: ItemRelativeStatsBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(data: RelativeStatsUiModel) {
+        with(binding) {
+            itemRelativeTvName.text = data.opponentName
+            itemRelativeTvStats.text = data.matchResult
+            itemRelativeTvLastMatch.text = data.lastMatchData
+            itemRelativeTvGoal.text =
+                itemView.context.getString(R.string.item_relative_stats_score, data.goal)
+            itemRelativeTvConceded.text =
+                itemView.context.getString(R.string.item_relative_stats_score, data.conceded)
+        }
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): RelativeStatsViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ItemRelativeStatsBinding.inflate(
+                layoutInflater,
+                parent,
+                false,
+            )
+            return RelativeStatsViewHolder(binding)
+        }
+    }
+}
