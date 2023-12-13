ccdecab2a87734863c844296c6ba8059f18e3f03
krrong <khk9664@naver.com>
2023-12-13 14:40:58 +0900

    feat: 리사이클러뷰 아이템 클릭 이벤트 추가

--- android/app/src/main/java/com/foss/foss/feature/statsearching/relative/RelativeStatsAdapter.kt
+++ android/app/src/main/java/com/foss/foss/feature/statsearching/relative/RelativeStatsAdapter.kt
@@ -1,5 +1,6 @@
 package com.foss.foss.feature.statsearching.relative
 
+import android.util.Log
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.foss.foss.model.RelativeStatUiModel
@@ -8,7 +9,9 @@ class RelativeStatsAdapter :
     ListAdapter<RelativeStatUiModel, RelativeStatsViewHolder>(RelativeStatDiffUtilCallBack()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RelativeStatsViewHolder {
-        return RelativeStatsViewHolder.from(parent)
+        return RelativeStatsViewHolder.from(parent) {
+            Log.d("krrong", getItem(it).opponentName)
+        }
     }
 
     override fun onBindViewHolder(holder: RelativeStatsViewHolder, position: Int) {
