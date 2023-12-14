819670e41c6ea7d2a86689d310a4238b229af010
krrong <khk9664@naver.com>
2023-12-14 13:18:08 +0900

    feat: 특정 유저와의 상대전적을 구해 저장하는 뷰모델 구현

--- android/app/src/main/java/com/foss/foss/feature/statsearching/relative/detail/RelativeDetailStatsViewModel.kt
+++ android/app/src/main/java/com/foss/foss/feature/statsearching/relative/detail/RelativeDetailStatsViewModel.kt
@@ -1,7 +1,29 @@
 package com.foss.foss.feature.statsearching.relative.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.foss.foss.model.MatchMapper.toUiModel
+import com.foss.foss.model.MatchUiModel
+import com.foss.foss.repository.MatchRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
-class RelativeDetailStatsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class RelativeDetailStatsViewModel(
+    private val matchRepository: MatchRepository,
+) : ViewModel() {
+    private val _matches: MutableStateFlow<List<MatchUiModel>> = MutableStateFlow(emptyList())
+    val matches: SharedFlow<List<MatchUiModel>>
+        get() = _matches.asSharedFlow()
+
+    fun fetchMatchesBetweenUsers(nickname: String, opponentNickname: String) {
+        viewModelScope.launch {
+            matchRepository.fetchMatchesBetweenUsers(nickname, opponentNickname)
+                .onSuccess {
+                    _matches.value = it.map { it.toUiModel() }
+                }
+                .onFailure { }
+        }
+    }
 }
