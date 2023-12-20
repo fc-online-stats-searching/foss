31701c442c5709b10a133e6eab608bcc32270b70
Jake <55350092+anjm1020@users.noreply.github.com>
2023-12-20 15:59:56 +0900

    feat: member, match 엔티티 설계 (#22)
    * feat: member, match 엔티티 설계
    
    * feat: 멤버 엔티티 추가 및 DDL 옵션 적용
    
    * feat: member table name 추가
    
    * feat: 도메인 상세 명세 추가

---
+++ server/src/main/java/com/foss/server/domain/member/Member.java
@@ -0,0 +1,48 @@
+package com.foss.server.domain.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Entity
+@Table(name = "members")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "mb_id", nullable = false)
+  private Long id;
+
+  @Setter(AccessLevel.PUBLIC)
+  @NotBlank(message = "사용자 닉네임은 1자 이상이어야 합니다")
+  @Column(name = "mb_username", nullable = false)
+  private String username;
+
+  @NotBlank(message = "accessid는 필수 입니다")
+  @Column(name = "mb_access_id", nullable = false)
+  private String accessId;
+
+  @Setter(AccessLevel.PUBLIC)
+  @NotNull(message = "사용자 레벨은 필수입니다")
+  @Column(name = "mb_level", nullable = false)
+  private Integer level;
+
+  @Builder
+  public Member(String username, String accessId, Integer level) {
+    this.username = username;
+    this.accessId = accessId;
+    this.level = level;
+  }
+}
