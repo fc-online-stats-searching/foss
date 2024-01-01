2b27fd5e59a20dcfa39f881dcfbe0f98d96786a3
boogiwoogi <kimjinuk1999@gmail.com>
2024-01-01 22:39:10 +0900

    refactor: uiState에 포함되어 있는 명칭을 변경한다.

--- android/app/src/main/java/com/foss/foss/feature/matchsearching/recent/RecentMatchesUiState.kt
+++ android/app/src/main/java/com/foss/foss/feature/matchsearching/recent/RecentMatchesUiState.kt
@@ -1,13 +1,12 @@
 package com.foss.foss.feature.matchsearching.recent
 
-import com.foss.foss.model.MatchTypeUiModel
 import com.foss.foss.model.MatchUiModel
 
 sealed interface RecentMatchesUiState {
 
-    data class Default(val matchTypes: List<MatchTypeUiModel>) : RecentMatchesUiState
+    object Empty : RecentMatchesUiState
 
     object Loading : RecentMatchesUiState
 
-    data class Success(val data: List<MatchUiModel>) : RecentMatchesUiState
+    data class RecentMatches(val matches: List<MatchUiModel>) : RecentMatchesUiState
 }
