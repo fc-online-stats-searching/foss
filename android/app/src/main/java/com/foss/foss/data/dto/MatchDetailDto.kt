1f4b13dd944fdd23c014eea66c6959a37b55a962
krrong <khk9664@naver.com>
2024-01-11 16:41:34 +0900

    refactor: SerialName 추가

--- android/app/src/main/java/com/foss/foss/data/dto/MatchDetailDto.kt
+++ android/app/src/main/java/com/foss/foss/data/dto/MatchDetailDto.kt
@@ -1,13 +1,20 @@
 package com.foss.foss.data.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MatchDetailDto(
+    @SerialName("possession")
     val possession: Int,
+    @SerialName("shootCount")
     val shootCount: Int,
+    @SerialName("effectiveShootCount")
     val effectiveShootCount: Int,
+    @SerialName("passCount")
     val passCount: Int,
+    @SerialName("passSuccessRate")
     val passSuccessRate: Int,
+    @SerialName("squads")
     val squads: List<SquadsDto>,
 )
