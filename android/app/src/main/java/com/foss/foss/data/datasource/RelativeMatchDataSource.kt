08385f8074a47bc207820418da63f4ef56bcd084
yoonhyeokCho <hyeok6640@gmail.com>
2024-01-13 16:37:31 +0900

    feat: 상대 전적 정보를 받아오는 DataSource를 만든다.

--- android/app/src/main/java/com/foss/foss/data/datasource/RelativeMatchDataSource.kt
+++ android/app/src/main/java/com/foss/foss/data/datasource/RelativeMatchDataSource.kt
@@ -2,14 +2,26 @@ package com.foss.foss.data.datasource
 
 import com.foss.foss.data.dto.RelativeMatchDTO
 import com.foss.foss.data.service.RelativeMatchService
+import java.io.IOException
 
 class RelativeMatchDataSource(
     private val relativeMatchService: RelativeMatchService
 ) {
+
     suspend fun fetchRelativeMatches(nickname: String): Result<RelativeMatchDTO> {
         try {
-            val relativeMatchDTO = relativeMatchService.fetchRelativeMatches(nickname)
-            return relativeMatchDTO
+            val response = relativeMatchService.fetchRelativeMatches(nickname)
+
+            if (response.isSuccessful) {
+                val body = response.body()
+                if (body != null) {
+                    return Result.success(body)
+                } else {
+                    return Result.failure(Exception("Response body is null"))
+                }
+            } else {
+                return Result.failure(IOException("Request failed with code: ${response.code()}"))
+            }
         } catch (e: Exception) {
             return Result.failure(e)
         }
