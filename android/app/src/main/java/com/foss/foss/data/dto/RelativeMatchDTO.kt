dcf4217516c1d6b96bf72d6f9d546da4ec6633c8
yoonhyeokCho <hyeok6640@gmail.com>
2024-01-13 16:36:06 +0900

    feat: 서버로부터 받아온 상대 전적 데이터를 받는 RelativeMatchDTO.kt를 만든다.

--- android/app/src/main/java/com/foss/foss/data/dto/RelativeMatchDTO.kt
+++ android/app/src/main/java/com/foss/foss/data/dto/RelativeMatchDTO.kt
@@ -1,57 +1,88 @@
 package com.foss.foss.data.dto
 
-import kotlinx.serialization.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 @Serializable
 data class RelativeMatchDTO(
+    @SerialName("memberInfo")
     val memberInfo: MemberInfo,
+    @SerialName("relativeMatchResponse")
     val relativeMatchResponse: List<RelativeMatchResponse>
 )
 
 @Serializable
 data class MemberInfo(
+    @SerialName("nickname")
     val nickname: String,
+    @SerialName("level")
     val level: Int,
+    @SerialName("renewal")
     val renewal: String
 )
 
 @Serializable
 data class RelativeMatchResponse(
+    @SerialName("lastDate")
     val lastDate: String,
+    @SerialName("opponentNickname")
     val opponentNickname: String,
+    @SerialName("win")
     val win: Int,
+    @SerialName("tie")
     val tie: Int,
+    @SerialName("lose")
     val lose: Int,
+    @SerialName("gain")
     val gain: Int,
+    @SerialName("loss")
     val loss: Int,
+    @SerialName("matchResponse")
     val matchResponse: List<MatchResponse>
 )
 
 @Serializable
 data class MatchResponse(
+    @SerialName("matchType")
     val matchType: Int,
+    @SerialName("timestamp")
     val timestamp: String,
+    @SerialName("result")
     val result: String,
+    @SerialName("goals")
     val goals: Map<String, Int>,
+    @SerialName("nickName")
     val nickName: String,
+    @SerialName("opponentNickname")
     val opponentNickname: String,
+    @SerialName("matchDetail")
     val matchDetail: MatchDetail
 )
 
 @Serializable
 data class MatchDetail(
+    @SerialName("possession")
     val possession: Int,
+    @SerialName("shootCount")
     val shootCount: Int,
+    @SerialName("effectiveShootCount")
     val effectiveShootCount: Int,
+    @SerialName("passCount")
     val passCount: Int,
+    @SerialName("passSuccessRate")
     val passSuccessRate: Int,
+    @SerialName("squads")
     val squads: List<Squad>
 )
 
 @Serializable
 data class Squad(
+    @SerialName("pid")
     val pid: Int,
+    @SerialName("position")
     val position: Int,
+    @SerialName("grade")
     val grade: Int,
+    @SerialName("spRating")
     val spRating: Double
 )
