eee6fdff48b8536dcf301d1fa36b080d33c138c4
ehdtndla123 <ehdtndla123@naver.com>
2024-02-11 01:16:01 +0900

    feat : MemberInfoResponseDto 추가

---
+++ server/src/main/java/com/foss/server/dto/member/MemberInfoResponseDto.java
@@ -0,0 +1,24 @@
+package com.foss.server.dto.member;
+
+import com.foss.server.domain.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class MemberInfoResponseDto {
+
+    private String nickname;
+    private int level;
+    private LocalDateTime renewal;
+
+    public static MemberInfoResponseDto from(Member member) {
+        return new MemberInfoResponseDto(
+                member.getUsername(),
+                member.getLevel(),
+                member.getRenewal()
+        );
+    }
+}
