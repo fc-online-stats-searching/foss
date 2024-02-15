e3d35444f0ec4326859be37dfd3a0c2b13a5a658
boogiwoogi <kimjinuk1999@gmail.com>
2024-02-15 17:43:36 +0900

    refactor: ktlint를 적용한다.

--- android/domain/src/main/java/com/foss/foss/repository/RelativeMatchRepository.kt
+++ android/domain/src/main/java/com/foss/foss/repository/RelativeMatchRepository.kt
@@ -4,7 +4,7 @@ import com.foss.foss.model.RelativeMatch
 
 interface RelativeMatchRepository {
 
-    suspend fun requestRefresh(nickname:String)
+    suspend fun requestRefresh(nickname: String)
 
     suspend fun fetchRelativeMatches(nickname: String): Result<List<RelativeMatch>>
 }
