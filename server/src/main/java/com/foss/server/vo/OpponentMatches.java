d6d2e1fa1213c33bba01e4f2b8b7aaf28f5bca5a
ehdtndla123 <ehdtndla123@naver.com>
2024-02-25 02:37:17 +0900

    feat : 상대전적 일급 컬렉션 추가

---
+++ server/src/main/java/com/foss/server/vo/OpponentMatches.java
@@ -0,0 +1,50 @@
+package com.foss.server.vo;
+
+import com.foss.server.domain.match.Match;
+import com.foss.server.dto.MatchMapper;
+import com.foss.server.dto.match.RelativeMatchResponseDto;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OpponentMatches {
+
+    private Map<String, List<Match>> opponentMatches;
+
+    public OpponentMatches(String ouid, List<Match> matches) {
+        this.opponentMatches = new HashMap<>();
+        extractOpponentMatches(ouid, matches);
+    }
+
+    public List<RelativeMatchResponseDto> getRelativeMatchResponses(String ouid, String nickname) {
+        List<RelativeMatchResponseDto> relativeMatchResponse = new ArrayList<>();
+        for (Map.Entry<String, List<Match>> entry : opponentMatches.entrySet()) {
+            String opponentOuid = entry.getKey();
+            List<Match> matchList = entry.getValue();
+
+            relativeMatchResponse.add(MatchMapper.getRelativeMatchResponseDto(ouid, opponentOuid, nickname, matchList));
+        }
+        return relativeMatchResponse;
+    }
+
+    private void extractOpponentMatches(String ouid, List<Match> matches) {
+        for (Match match : matches) {
+            List<String> team1 = match.getTeam1();
+            List<String> team2 = match.getTeam2();
+
+            addOpponentMatches(ouid, team1, team2, match);
+            addOpponentMatches(ouid, team2, team1, match);
+        }
+    }
+
+    private void addOpponentMatches(String ouid, List<String> team, List<String> opponentTeam, Match match) {
+        if (team.contains(ouid)) {
+            for (String opponent : opponentTeam) {
+                opponentMatches.computeIfAbsent(opponent, key -> new ArrayList<>()).add(match);
+            }
+        }
+    }
+
+}
