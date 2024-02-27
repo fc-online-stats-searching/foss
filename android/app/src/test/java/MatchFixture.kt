5d2c30b6d7a5232c9ae3d589497047516f6b1fe1
krrong <khk9664@naver.com>
2024-02-27 20:46:21 +0900

    test: 깨지는 테스트 수정

--- android/app/src/test/java/MatchFixture.kt
+++ android/app/src/test/java/MatchFixture.kt
@@ -2,18 +2,18 @@ import com.foss.foss.model.Match
 import com.foss.foss.model.MatchType
 import com.foss.foss.model.Score
 import com.foss.foss.model.WinDrawLose
-import java.time.LocalDate
+import java.time.LocalDateTime
 
 object MatchFixture {
 
     fun create() = List(3) {
         Match(
-            date = LocalDate.of(2023, 12, 18),
+            date = LocalDateTime.now(),
             matchType = MatchType.OFFICIAL,
             opponentName = "신공학관캣대디",
             manOfTheMatch = 1,
             winDrawLose = WinDrawLose.WIN,
-            score = Score(point = 31, otherPoint = 23)
+            score = Score(point = 31, otherPoint = 23),
         )
     }
 }
