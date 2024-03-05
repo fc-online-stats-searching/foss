a8ac19ecd90ae3a0e05b955611f3690d6e9176d8
krrong <khk9664@naver.com>
2024-03-05 22:04:28 +0900

    refactor: LazyColumn의 크기를 fillMaxSize로 변경한다.

--- android/app/src/main/java/com/foss/foss/feature/matchsearching/recent/NewRecentMatchActivity.kt
+++ android/app/src/main/java/com/foss/foss/feature/matchsearching/recent/NewRecentMatchActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -169,7 +170,7 @@ fun MatchCardColumn(
     data: List<MatchUiModel>,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(modifier = modifier) {
+    LazyColumn(modifier = modifier.fillMaxSize()) {
         items(data) { matchUiModel ->
             MatchCard(
                 matchUiModel = matchUiModel,
