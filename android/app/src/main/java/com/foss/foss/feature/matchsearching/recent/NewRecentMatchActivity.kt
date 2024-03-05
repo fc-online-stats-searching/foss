e64eb2feec3539679cfec4341dea4dd7b33f248e
krrong <khk9664@naver.com>
2024-03-05 22:02:07 +0900

    refactor: string을 리소스화 한다.

--- android/app/src/main/java/com/foss/foss/feature/matchsearching/recent/NewRecentMatchActivity.kt
+++ android/app/src/main/java/com/foss/foss/feature/matchsearching/recent/NewRecentMatchActivity.kt
@@ -74,12 +74,12 @@ class NewRecentMatchActivity : ComponentActivity() {
                         selected = selected,
                         list = types,
                         onSelectionChanged = { selected = it },
-                        modifier = Modifier.padding(top = 18.dp, bottom = 6.dp, end = 18.dp)
+                        modifier = Modifier.padding(top = 18.dp, bottom = 6.dp, end = 18.dp),
                     )
                     MatchCardColumn(
                         recentMatch.filter { match ->
                             selected == MatchTypeUiModel.ALL || match.matchType == selected
-                        }
+                        },
                     )
                 }
             }
@@ -90,7 +90,7 @@ class NewRecentMatchActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecentMatchScreen(
-    content: @Composable (modifier: Modifier) -> Unit
+    content: @Composable (modifier: Modifier) -> Unit,
 ) {
     Scaffold(
         topBar = {
@@ -98,7 +98,7 @@ fun RecentMatchScreen(
                 title = {
                     Text(
                         text = stringResource(id = R.string.common_recent_matches),
-                        color = colorResource(id = R.color.foss_wt)
+                        color = colorResource(id = R.color.foss_wt),
                     )
                 },
                 navigationIcon = {
@@ -106,7 +106,7 @@ fun RecentMatchScreen(
                         Icon(
                             imageVector = Icons.Filled.KeyboardArrowLeft,
                             contentDescription = null,
-                            tint = colorResource(R.color.foss_wt)
+                            tint = colorResource(R.color.foss_wt),
                         )
                     }
                 },
@@ -115,13 +115,13 @@ fun RecentMatchScreen(
                         Icon(
                             imageVector = Icons.Filled.Refresh,
                             contentDescription = null,
-                            tint = colorResource(R.color.foss_wt)
+                            tint = colorResource(R.color.foss_wt),
                         )
                     }
                 },
-                colors = TopAppBarDefaults.topAppBarColors(containerColor = colorResource(R.color.foss_bk))
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = colorResource(R.color.foss_bk)),
             )
-        }
+        },
     ) {
         content(modifier = Modifier.padding(it))
     }
@@ -129,7 +129,7 @@ fun RecentMatchScreen(
 
 @Composable
 fun SearchBar(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     TextField(
         shape = RoundedCornerShape(corner = CornerSize(10.dp)),
@@ -138,7 +138,7 @@ fun SearchBar(
         leadingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
-                contentDescription = null
+                contentDescription = null,
             )
         },
         colors = TextFieldDefaults.colors(
@@ -150,7 +150,7 @@ fun SearchBar(
             unfocusedPlaceholderColor = colorResource(id = R.color.foss_wt),
             focusedTextColor = colorResource(id = R.color.foss_wt),
             focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent
+            unfocusedIndicatorColor = Color.Transparent,
         ),
         placeholder = { Text(text = stringResource(id = R.string.common_request_searching_nickname)) },
         modifier = modifier
@@ -159,22 +159,22 @@ fun SearchBar(
             .heightIn(44.dp)
             .background(
                 color = colorResource(id = R.color.foss_gray800),
-                shape = RoundedCornerShape(corner = CornerSize(5.dp))
-            )
+                shape = RoundedCornerShape(corner = CornerSize(5.dp)),
+            ),
     )
 }
 
 @Composable
 fun MatchCardColumn(
     data: List<MatchUiModel>,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     LazyColumn(modifier = modifier) {
         items(data) { matchUiModel ->
             MatchCard(
                 matchUiModel = matchUiModel,
                 matchMvp = R.drawable.ic_player_example,
-                opponentTier = R.drawable.twenty
+                opponentTier = R.drawable.twenty,
             )
         }
     }
@@ -184,7 +184,7 @@ fun MatchCardColumn(
 fun MatchResult(
     point: Int,
     otherPoint: Int,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     val color: Color = if (point > otherPoint) {
         colorResource(id = R.color.foss_blue)
@@ -207,29 +207,29 @@ fun MatchResult(
                     topStart = CornerSize(5.dp),
                     topEnd = CornerSize(0.dp),
                     bottomStart = CornerSize(5.dp),
-                    bottomEnd = CornerSize(0.dp)
-                )
-            )
+                    bottomEnd = CornerSize(0.dp),
+                ),
+            ),
     ) {
         Text(
             text = result,
             color = colorResource(id = R.color.foss_wt),
             fontSize = 12.sp,
-            modifier = Modifier.padding(top = 18.dp)
+            modifier = Modifier.padding(top = 18.dp),
         )
         Divider(
             thickness = 1.dp,
             color = colorResource(id = R.color.foss_wt),
             modifier = Modifier
                 .padding(top = 4.dp)
-                .width(12.dp)
+                .width(12.dp),
         )
         Text(
             // todo : 8분동안 진행했다는 데이터가 넘어오면 수정 필요
             text = "08:00",
             color = colorResource(id = R.color.foss_wt),
             fontSize = 8.sp,
-            modifier = Modifier.padding(top = 4.dp, bottom = 18.dp, start = 6.dp, end = 6.dp)
+            modifier = Modifier.padding(top = 4.dp, bottom = 18.dp, start = 6.dp, end = 6.dp),
         )
     }
 }
@@ -239,7 +239,7 @@ fun MatchCard(
     matchUiModel: MatchUiModel,
     matchMvp: Int,
     opponentTier: Int,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
@@ -248,30 +248,30 @@ fun MatchCard(
             .padding(horizontal = 20.dp, vertical = 8.dp)
             .background(
                 color = colorResource(id = R.color.foss_gray900),
-                shape = RoundedCornerShape(corner = CornerSize(5.dp))
-            )
+                shape = RoundedCornerShape(corner = CornerSize(5.dp)),
+            ),
     ) {
         MatchResult(
             point = matchUiModel.point,
-            otherPoint = matchUiModel.otherPoint
+            otherPoint = matchUiModel.otherPoint,
         )
         MatchMvp(image = matchMvp)
         MatchOpponent(
             opponentName = matchUiModel.opponentName,
             opponentTier = opponentTier,
             point = matchUiModel.point,
-            otherPoint = matchUiModel.otherPoint
+            otherPoint = matchUiModel.otherPoint,
         )
         Column(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.End,
             modifier = Modifier
                 .weight(1f)
-                .padding(end = 12.dp)
+                .padding(end = 12.dp),
         ) {
             MatchType(
                 matchType = matchUiModel.matchType,
-                matchTime = matchUiModel.date
+                matchTime = matchUiModel.date,
             )
         }
     }
@@ -280,7 +280,7 @@ fun MatchCard(
 @Composable
 fun MatchMvp(
     modifier: Modifier = Modifier,
-    @DrawableRes image: Int
+    @DrawableRes image: Int,
 ) {
     Image(
         painter = painterResource(id = image),
@@ -288,7 +288,7 @@ fun MatchMvp(
         contentScale = ContentScale.Crop,
         alignment = Alignment.Center,
         modifier = modifier
-            .padding(start = 12.dp, top = 8.dp, bottom = 8.dp, end = 15.dp)
+            .padding(start = 12.dp, top = 8.dp, bottom = 8.dp, end = 15.dp),
     )
 }
 
@@ -298,13 +298,13 @@ fun MatchOpponent(
     @DrawableRes opponentTier: Int,
     point: Int,
     otherPoint: Int,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Column(modifier = modifier) {
         Text(
             text = stringResource(id = R.string.recent_match_opponent),
             fontSize = 12.sp,
-            color = colorResource(id = R.color.foss_gray100)
+            color = colorResource(id = R.color.foss_gray100),
         )
         Spacer(modifier = modifier.padding(vertical = 2.dp))
         Row {
@@ -314,14 +314,14 @@ fun MatchOpponent(
                 contentDescription = null,
                 modifier = Modifier
                     .height(16.dp)
-                    .width(16.dp)
+                    .width(16.dp),
             )
             Spacer(modifier = modifier.padding(horizontal = 1.dp))
             Text(
                 text = opponentName,
                 maxLines = 1,
                 fontSize = 12.sp,
-                color = colorResource(id = R.color.foss_wt)
+                color = colorResource(id = R.color.foss_wt),
             )
         }
         Spacer(modifier = modifier.padding(vertical = 2.dp))
@@ -329,14 +329,14 @@ fun MatchOpponent(
             Text(
                 text = stringResource(id = R.string.recent_match_match_score),
                 fontSize = 10.sp,
-                color = colorResource(id = R.color.foss_gray100)
+                color = colorResource(id = R.color.foss_gray100),
 
             )
             Spacer(modifier = modifier.padding(horizontal = 2.dp))
             Text(
                 text = "$point : $otherPoint",
                 fontSize = 10.sp,
-                color = colorResource(id = R.color.foss_wt)
+                color = colorResource(id = R.color.foss_wt),
 
             )
         }
@@ -347,27 +347,27 @@ fun MatchOpponent(
 fun MatchType(
     matchType: MatchTypeUiModel,
     matchTime: LocalDateTime,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Column(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.End,
-        modifier = modifier
+        modifier = modifier,
     ) {
         Text(
             text = when (matchType) {
-                MatchTypeUiModel.OFFICIAL -> "공식 경기"
-                MatchTypeUiModel.CLASSIC_ONE_TO_ONE -> "클래식 1on1"
-                else -> "전체 경기"
+                MatchTypeUiModel.OFFICIAL -> stringResource(id = R.string.common_official_description)
+                MatchTypeUiModel.CLASSIC_ONE_TO_ONE -> stringResource(id = R.string.common_classic_one_to_one)
+                else -> stringResource(id = R.string.common_all)
             },
             fontSize = 12.sp,
-            color = colorResource(id = R.color.foss_wt)
+            color = colorResource(id = R.color.foss_wt),
 
         )
         Text(
             text = matchTime.toTimeDiff(),
             fontSize = 10.sp,
-            color = colorResource(id = R.color.foss_gray100)
+            color = colorResource(id = R.color.foss_gray100),
         )
     }
 }
@@ -387,40 +387,40 @@ fun MatchTypeSpinner(
     selected: MatchTypeUiModel,
     list: List<MatchTypeUiModel>,
     onSelectionChanged: (selection: MatchTypeUiModel) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     var expanded by remember { mutableStateOf(false) }
 
     Column(
         horizontalAlignment = Alignment.End,
-        modifier = modifier.fillMaxWidth()
+        modifier = modifier.fillMaxWidth(),
     ) {
         Button(
             colors = ButtonDefaults.buttonColors(colorResource(R.color.foss_gray700)),
             contentPadding = PaddingValues(0.dp),
             shape = RoundedCornerShape(corner = CornerSize(5.dp)),
-            onClick = { expanded = !expanded }
+            onClick = { expanded = !expanded },
         ) {
             Row(
                 modifier = Modifier.padding(start = 12.dp),
-                verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.CenterVertically,
             ) {
                 Text(
                     color = Color.White,
                     textAlign = TextAlign.Center,
                     text = stringResource(id = selected.resId),
-                    fontSize = 12.sp
+                    fontSize = 12.sp,
                 )
                 Icon(
                     tint = colorResource(id = R.color.foss_wt),
                     painter = painterResource(id = R.drawable.ic_arrow_down),
-                    contentDescription = null
+                    contentDescription = null,
                 )
             }
             DropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded = false },
-                modifier = Modifier.background(colorResource(id = R.color.foss_gray700))
+                modifier = Modifier.background(colorResource(id = R.color.foss_gray700)),
             ) {
                 list.forEach { entry ->
                     DropdownMenuItem(
@@ -433,12 +433,12 @@ fun MatchTypeSpinner(
                                 text = stringResource(id = entry.resId),
                                 color = colorResource(R.color.foss_wt),
                                 fontSize = 12.sp,
-                                modifier = Modifier.align(Alignment.Start)
+                                modifier = Modifier.align(Alignment.Start),
                             )
                         },
                         modifier = Modifier
                             .fillMaxWidth()
-                            .background(colorResource(id = R.color.foss_gray700))
+                            .background(colorResource(id = R.color.foss_gray700)),
                     )
                 }
             }
@@ -460,12 +460,12 @@ fun RecentMatchScreenPreview() {
                 onSelectionChanged = {
                     selected = it
                 },
-                modifier = Modifier.padding(top = 18.dp, bottom = 6.dp, end = 18.dp)
+                modifier = Modifier.padding(top = 18.dp, bottom = 6.dp, end = 18.dp),
             )
             MatchCardColumn(
                 recentMatch.filter { match ->
                     selected == MatchTypeUiModel.ALL || match.matchType == selected
-                }
+                },
             )
         }
     }
