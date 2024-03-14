220d90720c5e2ddd81e9b318823159aeb1a4a40f
ehdtndla123 <ehdtndla123@naver.com>
2024-03-15 02:33:34 +0900

    feat : ResponseEntityExceptionHandler 추가하여 매개변수 누락 부분 ExceptionResponse 반환

--- server/src/main/java/com/foss/server/exception/ExceptionAdvice.java
+++ server/src/main/java/com/foss/server/exception/ExceptionAdvice.java
@@ -2,16 +2,23 @@ package com.foss.server.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static com.foss.server.exception.ExceptionType.*;
 
 
 @RequiredArgsConstructor
 @RestControllerAdvice
-public class ExceptionAdvice {
+public class ExceptionAdvice extends ResponseEntityExceptionHandler {
+
+
 
     @ExceptionHandler(MemberNotFoundException.class)
     protected ResponseEntity<ExceptionResponse> memberNotFoundException(HttpServletRequest request, Exception e) {
@@ -36,6 +43,13 @@ public class ExceptionAdvice {
     protected ResponseEntity<ExceptionResponse> nicknameNotFoundException(HttpServletRequest request, Exception e) {
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(NOT_FOUND_MEMBER_NICKNAME.getCode(), NOT_FOUND_MEMBER_NICKNAME.getMessage()));
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return ResponseEntity.badRequest()
+                .body(new ExceptionResponse(MISSING_REQUEST_PARAMETER.getCode(), MISSING_REQUEST_PARAMETER.getMessage()));
     }
 }
 
