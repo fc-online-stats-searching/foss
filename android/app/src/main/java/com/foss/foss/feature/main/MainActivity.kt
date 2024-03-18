15fe0ff9872bd36389a28f47a0eefc6178cad467
boogiwoogi <kimjinuk1999@gmail.com>
2024-03-18 23:10:09 +0900

    refactor: hilt를 적용한다.

--- android/app/src/main/java/com/foss/foss/feature/main/MainActivity.kt
+++ android/app/src/main/java/com/foss/foss/feature/main/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.foss.foss.design.FossTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
--- android/app/src/main/java/com/foss/foss/feature/matchsearching/relativematch/RelativeMatchViewModel.kt
+++ android/app/src/main/java/com/foss/foss/feature/matchsearching/relativematch/RelativeMatchViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.foss.foss.model.RelativeMatchMapper.toUiModel
 import com.foss.foss.repository.RelativeMatchRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
@@ -12,12 +13,16 @@ import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RelativeMatchViewModel(private val relativeMatchRepository: RelativeMatchRepository) :
-    ViewModel() {
+@HiltViewModel
+class RelativeMatchViewModel @Inject constructor(
+    private val relativeMatchRepository: RelativeMatchRepository
+) : ViewModel() {
 
     private val _uiState: MutableStateFlow<RelativeMatchUiState> =
         MutableStateFlow(RelativeMatchUiState.Default)
@@ -28,24 +33,22 @@ class RelativeMatchViewModel(private val relativeMatchRepository: RelativeMatchR
     val event: SharedFlow<RelativeMatchEvent>
         get() = _event.asSharedFlow()
 
-    fun fetchDefaultRelativeMatches() {
-        _uiState.value = RelativeMatchUiState.Default
-    }
-
     fun fetchRelativeMatches(nickname: String) {
         viewModelScope.launch {
-            _uiState.value = RelativeMatchUiState.Loading
-            relativeMatchRepository.fetchRelativeMatches(nickname)
-                .onSuccess { relativeMatches ->
-                    _uiState.value = if (relativeMatches.isEmpty()) {
-                        RelativeMatchUiState.Empty
-                    } else {
-                        RelativeMatchUiState.RelativeMatches(relativeMatches.map { it.toUiModel() })
-                    }
-                }.onFailure {
-                    _event.emit(RelativeMatchEvent.Failed)
-                    _uiState.value = RelativeMatchUiState.Default
+            flow {
+                emit(relativeMatchRepository.fetchRelativeMatches(nickname))
+            }.onStart {
+                _uiState.value = RelativeMatchUiState.Loading
+            }.catch {
+                _event.emit(RelativeMatchEvent.Failed)
+                _uiState.value = RelativeMatchUiState.Default
+            }.collect { relativeMatches ->
+                _uiState.value = if (relativeMatches.isEmpty()) {
+                    RelativeMatchUiState.Empty
+                } else {
+                    RelativeMatchUiState.RelativeMatches(relativeMatches.map { it.toUiModel() })
                 }
+            }
         }
     }
 
