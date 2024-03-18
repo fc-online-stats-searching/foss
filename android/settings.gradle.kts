eab0bd6452b2db02349e0cfb5e71f517858904ed
yoonhyeokCho <90389363+yoonhyeokCho@users.noreply.github.com>
2024-03-18 18:17:57 +0900

    refactor: gradle 파일을 kts 로 변경한다. (#67)

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -10,9 +10,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 rootProject.name = "foss"
-include ':app'
-include ':domain'
+include(":app")
+include(":domain")
